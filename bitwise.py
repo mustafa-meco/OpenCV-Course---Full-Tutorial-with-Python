cb387aa2d6f07643a9a9d1e6ba8aa6200ca1d623
Mustafa <musgamal05@gmail.com>
2022-05-27 09:20:40 +0200

    Masking

--- bitwise.py
+++ bitwise.py
@@ -7,7 +7,7 @@ rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
 circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
 
 cv.imshow('Rectangle', rectangle)
-cv.imshow('Circle', circle)\
+cv.imshow('Circle', circle)
 
 # bitwise AND --> intersection regions
 bitwise_and = cv.bitwise_and(rectangle, circle)
